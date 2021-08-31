8818a7fa401f3d4b6172b8dec51512d7c4bcbdd9
GirijalaAditya <girijalaaditya@gmail.com>
2021-08-31 15:37:04 +0530

    Added Code

---
+++ src/main/java/com/aditya/App.java
@@ -0,0 +1,14 @@
+package com.aditya;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "OM NAMAHA SHIVAYA" );
+        System.out.println( "OM NAMO NARAYANAYA" );
+    }
+}
